c5ec1652353efd83d7b2deac972f85aaf749f392
John Doe <johndoe@example.com>
2023-12-14 09:39:46 +0100

    model

---
+++ BookStoreAPI/Model/Book.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStorageAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+    }
+}
