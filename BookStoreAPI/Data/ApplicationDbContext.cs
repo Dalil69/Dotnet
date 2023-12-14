ec8c1f179c0567272e3cfdaa6115c42ea8290dd1
John Doe <johndoe@example.com>
2023-12-14 10:02:28 +0100

    data app

---
+++ BookStoreAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BookStorageAPI.Models;
+
+namespace DbContextAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
